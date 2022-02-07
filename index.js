06b720275fff9d5458c5d5c9dfb01f1d76fee6e5
eriuus <erik@uusitalo.io>
2022-02-07 23:18:59 +0100

    create new tiny urls and simple auth

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ import { Router } from 'itty-router'
 // now let's create a router (note the lack of "new")
 const router = Router()
 
+const auth = req => {
+  const apiToken = API_TOKEN
+  const reqToken = req.headers.get('Authorization')
+
+  if (!req.headers.has('Authorization')) {
+    return new Response('', { status: 400 })
+  }
+
+  if (apiToken != reqToken) {
+    return new Response('', { status: 400 })
+  }
+}
+
 // GET collection index
 router.get('/:id', async ({ params }) => {
   const { id } = params
@@ -19,6 +32,27 @@ router.get('/:id', async ({ params }) => {
   }
 
   return Response.redirect(destinationURL, 301)
+})
+
+router.post('/', auth, async req => {
+  const body = await req.json()
+
+  const destination = body.destination
+  const id = nanoid(parseInt(ID_LENGTH))
+
+  await TINY_URL.put(id, destination)
+
+  const json = JSON.stringify(
+    { tinyUrl: `https://${HOST}/${id}`, destination },
+    null,
+    2,
+  )
+
+  return new Response(json, {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  })
 })
 
 router.all('*', () => new Response('', { status: 404 }))
